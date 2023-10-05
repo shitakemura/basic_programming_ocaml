5eaaf3dc5c98f9d57f3c7609bb0c414f3c258e67
shitakemura <shitakemura@gmail.com>
2023-10-06 00:00:48 +0900

    ~ 5.4

---
+++ src/chapter5.ml
@@ -0,0 +1,14 @@
+(* 第5章 条件分岐 *)
+
+(* 5.1 条件分岐の必要性 *)
+
+(* 5.2 条件分岐の構文 *)
+if true then 1 else 2 ;;
+if false then 1 else 2 ;;
+
+if 1 < 2 then 3 else 4 ;;
+if 3 + 4 < 5 then 1 else 2 ;;
+
+(* 5.3 給与の例 *)
+
+(* 5.4 式としてのif文 *)
