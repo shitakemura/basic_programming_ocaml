ecf07aef59295a7be818dabd720880cc0f2467f6
shitakemura <shitakemura@gmail.com>
2023-10-24 07:50:48 +0900

    ~ 16.2

--- src/chpater16.ml
+++ src/chpater16.ml
@@ -9,7 +9,25 @@ type distance_t = {
 }
 
 (* 目的 : 先頭からリスト中の各点までの距離の合計を計算する *)
-let rec total_distance lst = match lst with
+(* let rec total_distance lst = match lst with
     [] -> []
   | {kyori = k; total = t} :: rest ->
-      []
+      [] *)
+  
+(* 16.2 アキュムレーター *)
+
+(* 目的 : 先頭からリスト中の各点までの距離の合計を計算する *)
+(* total0 : アキュムレータ *)
+(* let rec hojo lst total0 = match lst with
+    [] -> []
+  | {kyori = k; total = t} :: rest -> 
+      {kyori = k; total = total0 +. k} :: hojo rest (total0 +. k)
+
+let total_distance lst = hojo lst 0.0 *)
+
+let total_distance lst =
+  let rec hojo lst total0 = match lst with
+      [] -> []
+    | {kyori = k; total = t} :: rest ->
+        {kyori = k; total = total0 +. k} :: hojo rest (total0 +. k)
+    in hojo lst 0.0
