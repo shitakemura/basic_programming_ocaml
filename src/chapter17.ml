b57ee96f1d59d7cdc5eb82a4f48d45189420f371
shitakemura <shitakemura@gmail.com>
2023-10-25 08:58:35 +0900

    ~ 17.1

---
+++ src/chapter17.ml
@@ -0,0 +1,26 @@
+(* 第17章 再帰的なデータ構造 *)
+
+(* 17.1 バリアント型 *)
+type team_t = Red | White ;;
+Red ;;
+White ;;
+
+(* 目的 : 受け取ったチーム名を文字列で返す *)
+let team_setting team = match team with
+    Red -> "赤組"
+  | White -> "白組" ;;
+
+(* 年号を表す型 *)
+type nengou_t = Meiji of int
+              | Taisho of int
+              | Showa of int
+              | Heisei of int ;;
+Showa (42) ;;
+
+(* 目的 : 年号を受け取ったら対応する西暦年を返す *)
+let to_seireki nengou = match nengou with
+    Meiji (n) -> n + 1867
+  | Taisho (n) -> n + 1911
+  | Showa (n) -> n + 1925
+  | Heisei (n) -> n + 1988 ;;
+to_seireki (Showa (55)) ;;
