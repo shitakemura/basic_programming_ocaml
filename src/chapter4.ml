82caeca8563536d37755039c28ecdfba19957a9c
shitakemura <shitakemura@gmail.com>
2023-10-29 07:40:42 +0900

    ~ 4.1

---
+++ src/chapter4.ml
@@ -0,0 +1,9 @@
+(* 第4章 関数の定義 *)
+
+(* 4.1 関数定義の必要性 *)
+let jikyu = 950 ;;
+let kihonkyu = 100 ;;
+
+(25 * jikyu) + (28 * jikyu) + (31 * jikyu) ;;
+
+(kihonkyu + 25 * jikyu) + (kihonkyu + 28 * jikyu) + (kihonkyu + 31 * jikyu) ;;
