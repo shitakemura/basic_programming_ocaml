b310cc8a7131f8bea248a2bc97e48aafb0ff6339
shitakemura <shitakemura@gmail.com>
2023-10-28 11:19:20 +0900

    ~ 18.3

--- src/chapter18.ml
+++ src/chapter18.ml
@@ -27,3 +27,32 @@ let rec total_price yasai_list yaoya_list = match yasai_list with
     match price first yaoya_list with
         None -> total_price rest yaoya_list
       | Some (p) -> p + total_price rest yaoya_list ;;
+
+(* 18.3 オプション型を使った例外処理の問題点 *)
+
+(* 目的 : yasai_listを買ったときの値段の合計を調べる *)
+let rec total_price yasai_list yaoya_list = 
+  match yasai_list with
+      [] -> Some (0)
+    | first :: rest ->
+        match price first yaoya_list with
+            None -> None
+          | Some (p) -> match total_price rest yaoya_list with
+                            None -> None
+                          | Some (q) -> Some (p + q) ;;
+
+(* オプション型を使った例外処理の例 *)
+(* 目的 : yasai_listを買ったときの値段の合計を調べる *)
+let total_price yasai_list yaoya_list =
+  let rec hojo yasai_list = match yasai_list with
+      [] -> Some (0)
+    | first :: rest ->
+        match price first yaoya_list with
+            None -> None
+          | Some (p) -> match hojo rest with
+                            None -> None
+                          | Some (q) -> Some (p + q)
+  in match hojo yasai_list with
+      None -> 0
+    | Some (p) -> p ;;
+
