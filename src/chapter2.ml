d020e0df9c7268f44ae1151f7deee95d1594a3f1
shitakemura <shitakemura@gmail.com>
2023-10-28 14:26:11 +0900

    ~ 2.5

--- src/chapter2.ml
+++ src/chapter2.ml
@@ -26,3 +26,37 @@ infinity +. 3.0 ;;
 
 neg_infinity ;;
 3.0 -. neg_infinity ;;
+
+(* 2.3 文字列 *)
+
+"Yokohama" ;;
+"横浜" ;;
+
+"横浜" ^ "駅" ;;
+"新" ^ "横浜" ^ "駅" ;;
+
+"" ;;
+
+(* 2.4 真偽値 *)
+
+true ;;
+false ;;
+
+true && true ;;
+true && false ;;
+true || false ;;
+false || false ;;
+not true ;;
+not false ;;
+not (false || not false && not false) ;;
+
+2 < 3 ;;
+2 < 3 && 2.0 = 3.0 ;;
+2 < 3 || 2.0 = 3.0 ;;
+not (3 < 2) ;;
+
+true > false ;;
+false > true ;;
+true > true ;;
+
+(* 2.5 そのほかのデータ *)
