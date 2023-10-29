4e0eebd8fbbe92d61d093bc2bc1e838e2e1d1941
shitakemura <shitakemura@gmail.com>
2023-10-29 13:51:18 +0900

    ~ 5.2

---
+++ src/chapter5.ml
@@ -0,0 +1,11 @@
+(* 第5章 条件分岐 *)
+
+(* 5.1 条件分岐の必要性 *)
+let yugu_jikyu = 980 ;;
+
+(* 5.2 条件分岐の構文 *)
+if true then 1 else 2 ;;
+if false then 1 else 2 ;;
+
+if 1 < 2 then 3 else 4 ;;
+if 3 + 4 < 5 then 1 else 2 ;;
