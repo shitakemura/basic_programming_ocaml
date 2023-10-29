f7741dd86dd92f46755b6321c6efc2803b059767
shitakemura <shitakemura@gmail.com>
2023-10-29 09:35:32 +0900

    ~ 4.2

--- src/chapter4.ml
+++ src/chapter4.ml
@@ -7,3 +7,16 @@ let kihonkyu = 100 ;;
 (25 * jikyu) + (28 * jikyu) + (31 * jikyu) ;;
 
 (kihonkyu + 25 * jikyu) + (kihonkyu + 28 * jikyu) + (kihonkyu + 31 * jikyu) ;;
+
+(* 4.2 関数定義の構文 *)
+let f x = 3 * x ;;
+
+f 4 ;;
+
+let g x y = x * x + y * y - 4 ;;
+
+g 3 2 ;;
+
+let kyuyo x = kihonkyu + x * jikyu ;;
+
+kyuyo 25 + kyuyo 28 + kyuyo 31 ;;
