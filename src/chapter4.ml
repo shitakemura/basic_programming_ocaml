b0cbb9604256978270a5320714d8cc4520e2d12a
shitakemura <shitakemura@gmail.com>
2023-10-29 13:40:04 +0900

    ~ 4.6

--- src/chapter4.ml
+++ src/chapter4.ml
@@ -26,3 +26,36 @@ kyuyo 25 + kyuyo 28 + kyuyo 31 ;;
 (* 4.4 型推論と型チェック *)
 let f2 x = 3.0 *. x ;;
 f2 2.5 ;;
+
+(* 4.5 関数の実行方法 *)
+let jikyu = 950 ;;
+let kihonkyu = 100 ;;
+let kyuyo x = kihonkyu + x * jikyu ;;
+kyuyo 25 + kyuyo 28 + kyuyo 31 ;;
+
+(* 4.6 関数定義に対するデザインレシピ *)
+
+(* 目的 : 働いた時間xに応じたアルバイト代を計算する *)
+(* kyuyo : int -> int *)
+let kyuyo x = 0 ;;
+
+(* テスト *)
+let test1 = kyuyo 25 = 23850 ;;
+let test2 = kyuyo 28 = 26700 ;;
+let test3 = kyuyo 31 = 29550 ;;
+
+(* ----------------------- *)
+(* 時給（円） *)
+let jikyu = 950 ;;
+(* 基本給（円） *)
+let kihonkyu = 100 ;;
+
+(* 目的 : 働いた時間xに応じたアルバイト代を計算する *)
+(* kyuyo : int -> int *)
+let kyuyo x = kihonkyu + x * jikyu ;;
+
+(* テスト *)
+let test1 = kyuyo 25 = 23850 ;;
+let test2 = kyuyo 28 = 26700 ;;
+let test3 = kyuyo 31 = 29550 ;;
+(* ----------------------- *)
