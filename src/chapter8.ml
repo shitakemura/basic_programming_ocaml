9808e4ff5a980697efe0a28ac46680f29837b38c
shitakemura <shitakemura@gmail.com>
2023-11-01 07:11:27 +0900

    ~ 8.3

--- src/chapter8.ml
+++ src/chapter8.ml
@@ -12,3 +12,24 @@ type gakusei_t = {
 
 {namae = "asai"; tensuu = 70; seiseki = "B"} ;;
 {tensuu = 70; namae = "asai"; seiseki = "B"} ;;
+
+(* 8.3 レコードとパターンマッチ *)
+let tsuuchi gakusei = match gakusei with
+  {namae = n; tensuu = t; seiseki = s} ->
+    n ^ " さんは " ^ string_of_int t ^ " 点で、成績は " ^ s ^ " です。" ;; 
+
+tsuuchi {namae="asai"; tensuu=70; seiseki="B"} ;;
+
+let hyouka gakusei = match gakusei with
+  {namae = n; tensuu = t; seiseki = s} ->
+    {namae = n;
+     tensuu = t;
+     seiseki = if t >= 80 then "A"
+               else if t >= 70 then "B"
+               else if t >= 60 then "C"
+               else "D"
+    } ;;
+
+hyouka {namae="asai"; tensuu=70; seiseki=""} ;;
+
+tsuuchi (hyouka {namae="asai"; tensuu=70; seiseki=""}) ;;
