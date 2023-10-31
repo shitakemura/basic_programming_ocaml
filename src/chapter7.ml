23da2ee1be8111a0d7688e2f292e1caff3949b26
shitakemura <shitakemura@gmail.com>
2023-10-31 21:30:31 +0900

    ~ 7.2

--- src/chapter7.ml
+++ src/chapter7.ml
@@ -10,3 +10,18 @@
 ((3, "a"), 3.14) ;;
 
 3.14, 2.71 ;;
+
+(* 7.2 パターンマッチ *)
+match (3, 5) with
+  (a, b) -> a + b ;;
+
+let add pair = match pair with
+  (a, b) -> a + b ;;
+
+add (3, 5) ;;
+
+let add (a, b) = a + b ;;
+
+let add2 a b = a + b ;;
+
+add2 3 5 ;;
