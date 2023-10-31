68439ffbfcdd725c160cfd3034447579ae509c0b
shitakemura <shitakemura@gmail.com>
2023-10-31 21:35:58 +0900

    ~ 7.4

--- src/chapter7.ml
+++ src/chapter7.ml
@@ -25,3 +25,17 @@ let add (a, b) = a + b ;;
 let add2 a b = a + b ;;
 
 add2 3 5 ;;
+
+(* 7.3 構造データに対するデザインレシピ *)
+
+(* 目的 : ふたつの整数の組pairを受け取りその要素の和を返す *)
+(* add : int * int -> int *)
+let add pair = match pair with
+  (a, b) -> a + b ;;
+
+(* テスト *)
+let test1 = add (0, 0) = 0 ;;
+let test2 = add (3, 5) = 8 ;;
+let test3 = add (3, -5) = -2 ;;
+
+(* 7.4 パターンマッチの実行方法 *)
