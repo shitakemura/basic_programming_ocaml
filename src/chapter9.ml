e78453066c4cc731b55ef0ba2292953f6f919e22
shitakemura <shitakemura@gmail.com>
2023-11-02 22:19:09 +0900

    ~ 9.6

--- src/chapter9.ml
+++ src/chapter9.ml
@@ -57,3 +57,45 @@ let test1 = sum [] = 0 ;;
 let test2 = sum [2] = 2 ;;
 let test3 = sum [1; 3] = 4 ;;
 let test4 = sum [1; 2; 3; 4; 5; 6; 7; 8; 9; 10] = 55 ;;
+
+(* 9.6 テンプレートの複合 *)
+
+(* 学生ひとり分のデータ（名前、点数、成績）を表す型 *)
+type gakusei_t = {
+  namae : string;
+  tensuu : int;
+  seiseki : string;
+} ;;
+
+(* gakusei_t list型のデータの例 *)
+let lst1 = [] ;;
+let lst2 = [{namae = "asai"; tensuu = 70; seiseki = "B"}] ;;
+let lst3 = [{namae = "asai"; tensuu = 70; seiseki = "B"};
+            {namae = "kaneko"; tensuu = 80; seiseki = "A"}] ;;
+let lst4 = [{namae = "yoshida"; tensuu = 80; seiseki = "A"};
+            {namae = "asai"; tensuu = 70; seiseki = "B"};
+            {namae = "kaneko"; tensuu = 85; seiseki = "A"}] ;;
+
+(* 目的 : 学生リストlstのうち成績がAの人の数を返す *)
+(* count_A : gakusei_t list -> int *)
+let rec count_A lst = match lst with
+    [] -> 0
+  | first :: rest -> (match first with
+                        {namae = n; tensuu = t; seiseki = s}
+                          -> if s = "A" 
+                              then 1 + count_A rest
+                              else count_A rest
+                      ) ;;
+
+let rec count_A lst = match lst with
+    [] -> 0
+  | {namae = n; tensuu = t; seiseki = s} :: rest
+      -> if s = "A"
+          then 1 + count_A rest
+          else count_A rest ;;
+
+(* テスト *)
+let test1 = count_A lst1 = 0 ;;
+let test2 = count_A lst2 = 0 ;;
+let test3 = count_A lst3 = 1 ;;
+let test3 = count_A lst3 = 1 ;;
