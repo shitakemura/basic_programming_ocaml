8e408a8173760f84d21d3480e48e3cf5cbf4a149
shitakemura <shitakemura@gmail.com>
2023-11-03 14:05:12 +0900

    ~ 10.5

--- src/chapter10.ml
+++ src/chapter10.ml
@@ -86,3 +86,18 @@ let rec shukei lst = match lst with
       else if s = "B" then (a, b + 1, c, d)
       else if s = "C" then (a, b, c + 1, d)
       else (a, b, c, d + 1) ;;
+
+(* 10.5 ふたつのリストを結合する関数 *)
+
+(* 目的 : lst1とlst2を受け取りそれらを結合したリストを返す *)
+(* append : 'a list -> 'a list -> 'a list *)
+let rec append lst1 lst2 = match lst1 with
+    [] -> lst2
+  | first :: rest -> first :: append rest lst2 ;;
+
+(* テスト *)
+let test1 = append [] [] = [] ;;
+let test2 = append [] [1; 2] = [1; 2] ;;
+let test3 = append [1; 2] [] = [1; 2] ;;
+let test4 = append [1; 2] [3; 4] = [1; 2; 3; 4] ;;
+let test5 = append ["a"; "b"; "c"; "d"; "e"] ["f"; "g"] = ["a"; "b"; "c"; "d"; "e"; "f"; "g"] ;;
