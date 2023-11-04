f02d47297fde46f74b576261af341548b6fa8006
shitakemura <shitakemura@gmail.com>
2023-11-04 13:48:16 +0900

    ~ 13.2

--- src/chapter13.ml
+++ src/chapter13.ml
@@ -47,3 +47,44 @@ let count_A lst = count lst "A" ;;
 (* 目的 : 学生リストlstのうち成績がBの人の数を返す *)
 (* count_B : gakusei_t list -> int *)
 let count_B lst = count lst "B" ;;
+
+(* 13.2 関数の一般化とmap *)
+
+(* 目的 : 実数のリストlstを受け取り各要素の平方根のリストを返す *)
+(* map_sqrt : flost list -> float list *)
+let rec map_sqrt lst = match lst with
+    [] -> []
+  | first :: rest -> sqrt first :: map_sqrt rest ;;
+
+(* 目的 : 学生のリストlstを受け取り成績を受けたリストを返す *)
+(* map_hyouka : gakusei_t list -> gakusei_t list *)
+
+let hyouka gakusei = match gakusei with
+  {namae = n; tensuu = t; seiseki = s} ->
+    {namae = n;
+     tensuu = t;
+     seiseki = if t >= 80 then "A"
+               else if t >=70 then "B"
+               else if t >= 60 then "C"
+               else "D"} ;;
+
+let rec map_hyouka lst = match lst with
+    [] -> []
+  | first :: rest -> hyouka first :: map_hyouka rest ;;
+
+(* 目的 : 関数fとリストlstを受け取りfを施したリストを返す *)
+(* map : ('a -> 'b) -> 'a list -> 'b list *)
+let rec map f lst = match lst with
+    [] -> []
+  | first :: rest -> f first :: map f rest ;;
+
+map sqrt [2.0; 3.0] ;;
+map sin [2.0; 3.0] ;;
+
+(* 目的 : 実数のリストlstを受け取り各要素の平方根のリストを返す *)
+(* map_sqrt : float list -> float list *)
+let map_sqrt lst = map sqrt lst ;;
+
+(* 目的 : 学生リストlstを受け取り成績を入れたリストを返す *)
+(* map_hyouka : gakusei_t list -> gakusei_t list *)
+let map_hyouka lst = map hyouka lst ;;
