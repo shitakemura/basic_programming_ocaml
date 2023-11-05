8d7f04243646582b376606b26a4e8d67a1faca6d
shitakemura <shitakemura@gmail.com>
2023-11-05 09:11:12 +0900

    ~ 13.5

--- src/chapter13.ml
+++ src/chapter13.ml
@@ -110,3 +110,16 @@ let twice7 f = f (f 7) ;;
 let add3 x = x + 3 ;;
 
 twice7 add3 ;;
+
+(* 13.5 関数を返す関数 *)
+
+let twice f = 
+  let g x = f (f x)
+  in g ;;
+
+let times2 x = x * 2 ;;
+
+let add6 = twice add3 ;;
+
+add6 8 ;;
+add6 9 ;;
