4516fab7564fe4d0872ca3b1543386bd1d7a06e4
shitakemura <shitakemura@gmail.com>
2023-11-05 14:12:16 +0900

    ~ 14.2

--- src/chapter14.ml
+++ src/chapter14.ml
@@ -41,3 +41,53 @@ let is_positive n = n > 0 ;;
 (* 目的 : 受け取ったリストlstから正の要素のみを取り出す *)
 (* filter_positive : int list -> int list *)
 let filter_positive lst = filter is_positive lst ;;
+
+(* 14.2 各要素をまとめあげる関数 *)
+
+(* 目的 : 受け取ったリストlstの各要素の和を求める *)
+(* sum : int list -> int *)
+let rec sum lst = match lst with
+    [] -> 0
+  | first :: rest -> first + sum rest ;;
+
+(* 目的 : 受け取ったリストlstの長さを求める *)
+(* length : 'a list -> int *)
+let rec length lst = match lst with
+    [] -> 0
+  | first :: rest -> 1 + length rest ;;
+
+(* 目的 : lst1とlst2を受け取りそれらを結合したリストを返す *)
+(* append : 'a list -> 'a list -> 'a list *)
+let rec append lst1 lst2 = match lst1 with
+    [] -> lst2
+  | first :: rest -> first + append rest lst2 ;;
+
+(* 目的 : initから始めてlstの要素を右から順にfを施し込む *)
+(* fold_right : ('a -> 'b -> 'b) -> 'a list -> 'b -> 'b *)
+let rec fold_right f lst init = match lst with
+    [] -> init
+  | first :: rest -> f first (fold_right f rest init) ;;
+
+(* 目的 : firstとrest_resultを加える *)
+(* add_int : int -> int -> int *)
+let add_int first rest_result = first + rest_result ;;
+
+(* 目的 : 受け取ったlstの各要素の和を求める *)
+(* sum : int list -> int *)
+let sum lst = fold_right add_int lst 0 ;;
+
+(* 目的 : firstは無視してrest_resultに1を加える *)
+(* add_one : int -> int -> int *)
+let add_one first rest_result = 1 + rest_result ;;
+
+(* 目的 : 受け取ったlstの長さを求める *)
+(* length: 'a list -> int *)
+let length lst = fold_right add_one lst 0 ;;
+
+(* 目的 : firstをリストrest_resultの先頭に加える *)
+(* cons : 'a -> 'a list -> 'a list *)
+let cons first rest_result = first :: rest_result ;;
+
+(* 目的 : lst1とlst2を受け取りそれらを結合したリストを返す *)
+(* append : 'a list -> 'a list -> 'a list *)
+let append lst1 lst2 = fold_right cons lst1 lst2 ;;
