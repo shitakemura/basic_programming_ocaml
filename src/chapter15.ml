e54b42cc475876fa2c3210c79749d0fad6b97250
shitakemura <shitakemura@gmail.com>
2023-11-07 07:50:38 +0900

    ~ 15.3

--- src/chapter15.ml
+++ src/chapter15.ml
@@ -48,3 +48,47 @@ let rec quick_sort lst = match lst with
   | first :: rest -> quick_sort (take_less first rest)
                      @ [first]
                      @ quick_sort (take_greater first rest) ;;
+
+(* 15.3 補助関数の作成 *)
+
+(* 目的 : lstの中からnより小さい要素のみを取り出す *)
+(* take_less : int -> int list -> int list *)
+let rec take_less n lst = match lst with
+    [] -> []
+  | first :: rest -> if first < n
+                     then first :: take_less n rest
+                     else take_less n rest ;;
+
+(* 目的 : lstの中からnより大きい要素のみを取り出す *)
+(* take_greater : int -> int list -> int list *)
+let rec take_greater n lst = match lst with
+    [] -> []
+  | first :: rest -> if first > n
+                     then first :: take_greater n rest
+                     else take_greater n rest ;;
+
+(* 目的 : lstの中からnより小さい要素のみを取り出す *)
+(* take_less : int -> int list -> int list *)
+let take_less n lst = List.filter (fun item -> item < n) lst ;;
+
+(* 目的 : lstの中からnより大きい要素のみを取り出す *)
+(* take_greater : int -> int list -> int list *)
+let take_greater n lst = List.filter (fun item -> item > n) lst ;;
+
+(* 目的 : 受け取ったlstをクイックソートを使って昇順に整列する *)
+(* quick_sort : int list -> int list *)
+let rec quick_sort lst =
+  (* 目的 : lstの中からnよりpである要素のみを取り出す *)
+  (* take : int -> int list -> (int -> int -> bool) -> int list *)
+  let take n lst p = List.filter (fun item -> p item n) lst
+  (* 目的 : lstの中からnより小さい要素のみを取り出す *)
+  (* take_less : int -> int list -> int list *)
+  in let take_less n lst = take n lst (<)
+  (* 目的 : lstの中からnより大きい要素のみを取り出す *)
+  (* take_greater : int -> int list -> int list *)
+  in let take_greater n lst = take n lst (>)
+  in match lst with
+      [] -> []
+    | first :: rest -> quick_sort (take_less first rest)
+                       @ [first]
+                       @ quick_sort (take_greater first rest) ;;
