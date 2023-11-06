b4399012ea65dd94455557405c7a63fd9c32a097
shitakemura <shitakemura@gmail.com>
2023-11-06 21:59:49 +0900

    ~ 15.2

--- src/chapter15.ml
+++ src/chapter15.ml
@@ -6,3 +6,45 @@
 let rec sum lst = match lst with
     [] -> 0
   | first :: rest -> first + sum rest ;;
+
+(* 15.2 部分問題の生成 *)
+
+(* 目的 : 受け取ったlstをクイックソートを使って昇順に整列する *)
+(* quick_sort : int list -> int list *)
+let rec quick_sort lst = [] ;;
+
+(* テスト *)
+let test1 = quick_sort [] = [] ;;
+let test2 = quick_sort [1] = [1] ;;
+let test3 = quick_sort [1; 2] = [1; 2] ;;
+let test4 = quick_sort [2; 1] = [1; 2] ;;
+let test5 = quick_sort [5; 4; 9; 8; 2; 3] = [2; 3; 4; 5; 8; 9] ;;
+
+(* 目的 : 受け取ったlstをクイックソートを使って昇順に整列する *)
+(* quick_sort : int list -> int list *)
+let rec quick_sort lst =
+  if lst = []
+    then []
+    else [] ;;
+
+(* 目的 : 受け取ったlstをクイックソートを使って昇順に整列する *)
+(* quick_sort : int list -> int list *)
+let rec quick_sort lst = match lst with
+    [] -> []
+  | first :: rest -> [] ;;
+
+(* 目的 : lstの中からnより小さい要素のみを取り出す *)
+(* take_less : int -> int list -> int list *)
+let take_less n lst = [] ;;
+
+(* 目的 : lstの中からnより大きい要素のみを取り出す *)
+(* take_greater : int -> int list -> int list *)
+let take_greater n lst = [] ;;
+
+(* 目的 : 受け取ったlstをクイックソートを使って昇順に整列する *)
+(* quick_sort : int list -> int list *)
+let rec quick_sort lst = match lst with
+    [] -> []
+  | first :: rest -> quick_sort (take_less first rest)
+                     @ [first]
+                     @ quick_sort (take_greater first rest) ;;
