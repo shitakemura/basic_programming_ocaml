445edc512d047049f6dd77471e89d3e0d506acca
shitakemura <shitakemura@gmail.com>
2023-11-07 21:49:27 +0900

    ~ 16.1

---
+++ src/chapter16.ml
@@ -0,0 +1,16 @@
+(* 第16章 情報の蓄積 *)
+
+(* 16.1 情報の欠落 *)
+
+(* 距離と距離の合計を持っている型 *)
+type distance_t = {
+  kyori : float;
+  total : float;
+} ;;
+
+(* 目的 : 先頭からリスト中の各点までの距離の合計を計算する *)
+(* total_distance : distance_t list -> distance_t list *)
+let rec total_distance lst = match lst with
+    [] -> []
+  | {kyori = k; total = t} :: rest ->
+    [] ;;
