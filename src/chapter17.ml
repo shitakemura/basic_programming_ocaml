4723c223ee3a0e45f647579d1bd1a90d5b385829
shitakemura <shitakemura@gmail.com>
2023-11-09 22:20:57 +0900

    ~ p187

--- src/chapter17.ml
+++ src/chapter17.ml
@@ -73,3 +73,37 @@ let rec sum_tree tree = match tree with
     Empty -> 0
   | Leaf (n) -> n
   | Node (t1, n, t2) -> sum_tree t1 + n + sum_tree t2 ;;
+
+(* 17.4 2分探索木 *)
+
+(* 目的 : dataが2分探索木treeに含まれているかを調べる *)
+(* search : tree_t -> int -> bool *)
+let rec search tree data = false ;;
+
+(* 2分探索木の例 *)
+let tree1 = Empty ;;
+let tree2 = Leaf (3) ;;
+let tree3 = Node (Leaf (1), 2, Leaf (3)) ;;
+let tree4 = Node (Empty, 7, Leaf (9)) ;;
+let tree5 = Node (tree3, 6, tree4) ;;
+
+(* テスト *)
+let test1 = search tree1 3 = false ;;
+let test2 = search tree2 3 = true ;;
+let test3 = search tree2 4 = false ;;
+let test4 = search tree5 6 = true ;;
+let test5 = search tree5 2 = true ;;
+let test6 = search tree5 1 = true ;;
+let test7 = search tree5 4 = false ;;
+let test8 = search tree5 7 = true ;;
+let test9 = search tree5 8 = false ;;
+
+(* 目的 : dataが2分探索木treeに含まれているかどうかを調べる *)
+(* search : tree_t -> int -> bool *)
+let rec search tree data = match tree with
+    Empty -> false
+  | Leaf (n) -> data = n
+  | Node (t1, n, t2) ->
+      if data = n then true
+      else if data < n then search t1 data
+      else search t2 data ;;
