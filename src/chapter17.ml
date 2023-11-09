b716d0337a67dc15226fb580dde2a62c7e32c13e
shitakemura <shitakemura@gmail.com>
2023-11-10 08:17:15 +0900

    ~ 17.4

--- src/chapter17.ml
+++ src/chapter17.ml
@@ -107,3 +107,29 @@ let rec search tree data = match tree with
       if data = n then true
       else if data < n then search t1 data
       else search t2 data ;;
+
+(* 目的 : 2分探索木treeにdataを追加した2分探索木を返す *)
+(* insert_tree : tree_t -> int -> tree_t *)
+let rec insert_tree tree data = Empty ;;
+
+(* テスト *)
+let test1 = insert_tree Empty 3 = Leaf (3) ;;
+let test2 = insert_tree (Leaf (3)) 2 = Node (Leaf (2), 3, Empty) ;;
+let test3 = insert_tree (Leaf (3)) 3 = Leaf (3) ;;
+let test4 = insert_tree (Leaf (3)) 4 = Node (Empty, 3, Leaf (4)) ;;
+let test5 = insert_tree tree5 4 = Node (Node (Leaf (1), 2, Node (Empty, 3, Leaf (4))),
+                                        6,
+                                        Node (Empty, 7, Leaf (9))) ;;
+
+(* 目的 : 2分探索木treeにdataを追加した2分探索木を返す *)
+(* insert_tree : tree_t -> int -> tree_t *)
+let rec insert_tree tree data = match tree with
+    Empty -> Leaf (data)
+  | Leaf (n) -> if data = n then Leaf (n)
+                else if data < n then Node (Leaf (data), n, Empty)
+                else Node (Empty, n, Leaf (data))
+  | Node (t1, n, t2) ->
+    if data = n then Node (t1, n, t2)
+    else if data < n then Node (insert_tree t1 data, n, t2)
+    else Node (t1, n, insert_tree t2 data) ;;
+
