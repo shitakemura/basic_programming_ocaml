907ae2601abddf5e31605c8022e4e3820029ae79
shitakemura <shitakemura@gmail.com>
2023-11-10 21:16:16 +0900

    ~ 18.3

--- src/chapter18.ml
+++ src/chapter18.ml
@@ -39,3 +39,52 @@ let rec total_price yasai_list yaoya_list = match yasai_list with
     match price first yaoya_list with
         None -> total_price rest yaoya_list
       | Some (p) -> p + total_price rest yaoya_list ;;
+
+(* 18.3 オプション型を使った例外処理の問題点 *)
+
+(* 目的 : yasai_listを買ったときの値段の合計を調べる *)
+(* total_price : string list -> (string * int) list -> int option *)
+let rec total_price yasai_list yaoya_list = match yasai_list with
+    [] -> Some (0)
+  | first :: rest ->
+    match price first yaoya_list with
+        None -> None
+      | Some (p) -> match total_price rest yaoya_list with
+                        None -> None
+                      | Some (q) -> Some (p + q) ;;
+
+(* オプション型を使った例外処理の例 *)
+(* 目的 : yasai_listを買ったときの値段の合計を調べる *)
+(* total_price : string list -> (string * int) list -> int *)
+let total_price yasai_list yaoya_list =
+  (* 目的 : yasai_listを買ったときの値段の合計を調べる *)
+  (* hojo : string lit -> int option *)
+  let rec hojo yasai_list = match yasai_list with
+      [] -> Some (0)
+    | first :: rest ->
+        match price first yaoya_list with
+            None -> None
+          | Some (p) -> match hojo rest with
+                            None -> None
+                          | Some (q) -> Some (p + q)
+  in match hojo yasai_list with
+      None -> 0
+    | Some (p) -> p ;;
+
+total_price ["たまねぎ"; "にんじん"] yaoya_list ;;
+total_price ["たまねぎ"; "じゃがいも"; "にんじん"] yaoya_list ;;
+
+(* 目的 : itemの値段を調べる *)
+(* price : string -> (string * int) list -> int *)
+let rec price item yaoya_list = match yaoya_list with
+    [] -> 0
+  | (yasai, nedan) :: rest ->
+      if item = yasai then nedan
+                      else price item rest ;;
+
+(* 目的 : yasai_listを買ったときの値段の合計を調べる *)
+(* total_price : string list -> (string * int) list -> int *)
+let rec total_price yasai_list yaoya_list = match yasai_list with
+    [] -> 0
+  | first :: rest ->
+      price first yaoya_list + total_price rest yaoya_list ;;
