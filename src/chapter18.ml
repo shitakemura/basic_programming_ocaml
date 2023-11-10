d1b87988a95563a262445bfac036c7ed2106df78
shitakemura <shitakemura@gmail.com>
2023-11-11 08:56:42 +0900

    ~ 18.5

--- src/chapter18.ml
+++ src/chapter18.ml
@@ -98,3 +98,33 @@ raise Urikire ;;
 
 exception Urikire of string ;;
 raise (Urikire ("じゃがいも")) ;;
+
+(* 18.5 例外処理の実際 *)
+
+(* 売り切れを示す例外 *)
+exception Urikire ;;
+
+(* 目的 : itemの値段を調べる *)
+(* みつからないときにはUrikireという例外を発生する *)
+(* price : string -> (string * int) list -> int *)
+let rec price item yaoya_list = match yaoya_list with
+    [] -> raise Urikire
+  | (yasai, nedan) :: rest ->
+      if item = yasai then nedan
+                      else price item rest ;;
+
+(* 目的 : yasai_listを買ったときの値段の合計を調べる *)
+(* total_price : string list -> (string * int) list -> int *)
+let total_price yasai_list yaoya_list =
+  (* 目的 : yasai_listを買ったときの値段の合計を調べる *)
+  (* hojo : string list -> int *)
+  let rec hojo yasai_list = match yasai_list with
+      [] -> 0
+    | first :: rest ->
+        price first yaoya_list + hojo rest
+  in try
+      hojo yasai_list
+  with Urikire -> 0 ;;
+
+total_price ["たまねぎ"; "にんじん"] yaoya_list ;;
+total_price ["たまねぎ"; "じゃがいも"; "にんじん"] yaoya_list ;;
